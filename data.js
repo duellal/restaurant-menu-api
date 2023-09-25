c7e465bd3fda06e5a51c7303232252adf3a51b48
Alex <alexandriaduell@gmail.com>
2023-09-25 15:59:18 -0400

    Started adding data - will continue when I work on next

---
+++ data.js
@@ -0,0 +1,47 @@
+let data = {
+    menu: [
+        appetizers: [
+            {id: 1, name: "Iceberg Wedge Salad with House Cured Bacon", ingredients: " tomato salsa gorgonzola", price: 7.50}
+            {},
+            {},
+            {},
+            {}
+        ],
+        entrees: [
+            {},
+            {},
+            {},
+            {}
+        ],
+        sandwiches: [
+            options: [
+                bread: [
+                    {id: , name: 'sourdough'},
+                    {id: , name: 'whole wheat'},
+                    {id: , name: 'rye'}
+                    {id: , name: 'chees & onion'}
+                ],
+                cold: [
+                    {id: , name: 'turkey & avocado', ingredients: ['tomato']}
+                    {id: , name: 'pub club', ingredients: ['turkey', 'bacon', 'lettuce', 'tomato']},
+                    {id: , name: 'rare roast beef & swiss', ingredients: ['swee-hot mustard', 'lettuce', 'red onion']},
+                    {id: , name: 'veggie', ingredients: ['pepper jack', 'avocado', 'sprout', 'tomato']}
+                ],
+                hot: [
+
+                ],
+                prices:[ 
+                    {id: , name: 'half', price: 7.95},
+                    {id: , name: 'full', price: 9.25}
+                ]
+            ]
+            
+        ],
+        soup-salads: [],
+        fajitas: [],
+        tacos: [],
+        enchiladas: [],
+        quiche: [],
+        salads: []
+    ]
+}
